d025fb12f2f81b6ae852a484f300606cd318be90
ksuares1 <kimberlyjsuares@gmail.com>
2023-03-22 22:45:09 -0400

    added code to html/css files

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// ⏰Feature #1
+// In your project, display the current date and time using JavaScript: Tuesday 16:00
+
+// 🕵️‍♀️Feature #2
+// Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
+
+// 🙀Bonus Feature
+// Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit. When clicking on it, it should convert the temperature to Fahrenheit. When clicking on Celsius, it should convert it back to Celsius.
+
+let currentTime = new Date();
+
+
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[date.getDay()];
+  let formattedDate = `Today is ${currentDay},  ${currentTime.getMilliseconds}`;
+
+  return formattedDate;
+}
+
+console.log(formatDate(currentTime));
